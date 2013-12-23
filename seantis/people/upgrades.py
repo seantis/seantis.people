0f39436db8780cebf1a08666545da5c6227fd94c
Denis Krienbühl <denis@href.ch>
2013-12-23 13:55:17 +0100

    Adds function to easily upgrade any portal_type using seantis.people's supermodels

--- seantis/people/upgrades.py
+++ seantis/people/upgrades.py
@@ -1,4 +1,9 @@
+from zope.component import getUtility
+
 from plone import api
+from plone.dexterity.interfaces import IDexterityFTI
+
+from seantis.people.supermodel.indexing import update_related_indexes
 
 
 def import_profile(profile):
@@ -6,20 +11,23 @@ def import_profile(profile):
     setup.runAllImportStepsFromProfile(profile)
 
 
-def run_import_step_from_profile(
-    context, step, module='seantis.people', profile='default'
-):
+def run_import_step_from_profile(step, module, profile):
     setup = api.portal.get_tool('portal_setup')
     profile = 'profile-{}:{}'.format(module, profile)
     setup.runImportStepFromProfile(profile, step)
 
 
+def upgrade_portal_type(portal_type, module, profile):
+    run_import_step_from_profile('typeinfo', module, profile)
+    update_related_indexes(getUtility(IDexterityFTI, portal_type))
+
+
 def install_custom_controlpanel(context):
-    run_import_step_from_profile(context, 'controlpanel')
+    run_import_step_from_profile('controlpanel', 'seantis.people', 'default')
 
 
 def upgrade_phz_type_info(context):
-    run_import_step_from_profile(context, 'typeinfo', 'seantis.people', 'phz')
+    upgrade_portal_type('seantis.people.phz', 'seantis.people', 'phz')
 
 
 def install_membership_dependencies(context):
