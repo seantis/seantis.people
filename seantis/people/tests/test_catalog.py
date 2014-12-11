3c4896f90d73e5528a7bedee90e891f0ffbb0dc0
Denis Krienbühl <denis@href.ch>
2014-12-11 16:58:38 +0100

    Adds a test ensuring that people metadata is only written to the people catalog

--- seantis/people/tests/test_catalog.py
+++ seantis/people/tests/test_catalog.py
@@ -1,15 +1,13 @@
 from plone import api
 from seantis.people import tests
 from seantis.people.interfaces import IPerson
+from seantis.people.supermodel import set_columns
 from seantis.people.supermodel.indexing import on_type_modified
 
 
 class TestCatalog(tests.IntegrationTestCase):
 
     def test_require_baseclass(self):
-        portal_catalog = api.portal.get_tool('portal_catalog')
-        people_catalog = api.portal.get_tool('seantis_people_catalog')
-
         without_baseclass = self.new_temporary_type(
             behaviors=[IPerson.__identifier__],
         )
@@ -38,6 +36,8 @@ class TestCatalog(tests.IntegrationTestCase):
             )
 
         path = '/'.join(container.getPhysicalPath())
+        portal_catalog = api.portal.get_tool('portal_catalog')
+        people_catalog = api.portal.get_tool('seantis_people_catalog')
 
         self.assertEqual(
             [b.id for b in portal_catalog(path={'query': path, 'depth': 1})],
@@ -48,3 +48,28 @@ class TestCatalog(tests.IntegrationTestCase):
             [b.id for b in people_catalog(path={'query': path, 'depth': 1})],
             ['with-baseclass']
         )
+
+    def test_column_metadata(self):
+        new_type = self.new_temporary_type(
+            behaviors=[IPerson.__identifier__],
+            klass='seantis.people.types.base.PersonBase'
+        )
+
+        set_columns(new_type.lookupSchema(), [['foo']])
+        on_type_modified(new_type)
+
+        with self.user('admin'):
+            api.content.create(
+                id='123',
+                type=new_type.id,
+                container=self.new_temporary_folder(),
+                foo='bar',
+            )
+
+        portal_catalog = api.portal.get_tool('portal_catalog')
+        people_catalog = api.portal.get_tool('seantis_people_catalog')
+
+        with self.assertRaises(AttributeError):
+            portal_catalog(portal_type=new_type.id)[0].foo
+
+        self.assertEqual(people_catalog(portal_type=new_type.id)[0].foo, 'bar')
