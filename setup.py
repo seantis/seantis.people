4f5f5ff6cd806bdad912ddac3f68a682dc3910d9
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2015-01-09 17:57:52 +0100

    Prepare next release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ name = 'seantis.people'
 description = (
     "A list of people, optionally organized by organizations and positions."
 )
-version = '0.24'
+version = '0.25'
 
 tests_require = [
     'plone.app.testing',
