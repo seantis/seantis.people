aae1f5481f81dd944a49ec418d788b95fbb1bf76
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2015-04-09 10:07:13 +0200

    Back to development

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ name = 'seantis.people'
 description = (
     "A list of people, optionally organized by organizations and positions."
 )
-version = '0.30'
+version = '0.31'
 
 tests_require = [
     'plone.app.testing',
